1687d7e9ed3ad05a9da87d18afd1181b5a8b74d6
Abhaybaby <167125342+Abhaybaby@users.noreply.github.com>
2024-04-23 22:16:04 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24643568"
 api_hash = "5c3ea512a2730bfab77e3d75f6272e7b"
-bot_token = "7053479766:AAHRCbHNHtde2bihRnOUmthzKmy7IzO_LTk"
+bot_token = "7000209903:AAFSNBkzZbbfpEbOs8Eps6e9rtErht9d-q8"
